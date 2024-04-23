61919e96b379596ecb01335ff58b39a35ce8c3de
igor <igorag314@gmail.com>
2024-04-23 14:26:57 -0300

    feat: adjusts to deploy

--- flask-api/app.py
+++ flask-api/app.py
@@ -3,14 +3,10 @@ from src.safe_browsing_google.safe_browsing_google import safe_browsing_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.register_blueprint(safe_browsing_bp, url_prefix='/api/url-check')
 
 CORS(app)
-
-app.register_blueprint(safe_browsing_bp, url_prefix='/api/url-check')
 
 @app.route('/')
 def index():
     return 'Api to check if url is safe.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
